c63beeab92e2c97e9fd73dfe8889d5ad6417f593
Ohlavrac <joaovicrcarvalho@gmail.com>
2021-06-29 09:40:24 -0300

    Resolvido problema com objetos nulos

--- src/Main.java
+++ src/Main.java
@@ -5,46 +5,52 @@ public class Main {
     static Vetor listaAlunos = new Vetor();
 
     public static void menu() {
-        System.out.println("Buscar...         [1]");
-        System.out.println("Remover...        [2]");
-        System.out.println("Mostrar Lista...  [3]");
+        System.out.println("Adicionar...      [1]");
+        System.out.println("Buscar...         [2]");
+        System.out.println("Remover...        [3]");
+        System.out.println("Mostrar Lista...  [4]");
+        System.out.println("Tamanho Lista...  [5]");
         System.out.println("Sair...           [0]");
     }
 
     public static void main(String args[]) {
         String alunoNome;
         double alunoNota;
-        int contador, numero = -1, posicao;
+        int numero = -1, posicao;
 
-        for (contador = 0; contador < listaAlunos.aluno.length; contador++) {
-            System.out.print("Informe o nome do aluno: ");
-            alunoNome = scan.next();
-            System.out.print("Informe a nota do aluno: ");
-            alunoNota = scan.nextDouble();
-            Aluno alunos = new Aluno(alunoNome, alunoNota);
-            listaAlunos.adicionar(alunos);
-        }
 
         while (numero != 0) {
             menu();
             System.out.print(">>>: ");
             numero = scan.nextInt();
-            System.out.flush();
             switch (numero) {
                 case 1:
-                    System.out.println("Informe a posicao deseja: ");
-                    posicao = scan.nextInt();
-                    listaAlunos.buscar(posicao);
+                    System.out.print("Informe o nome do aluno: ");
+                    alunoNome = scan.next();
+                    System.out.print("Informe a nota do aluno: ");
+                    alunoNota = scan.nextDouble();
+                    Aluno alunos = new Aluno(alunoNome, alunoNota);
+                    listaAlunos.adicionar(alunos);
                     break;
 
                 case 2:
                     System.out.println("Informe a posicao deseja: ");
                     posicao = scan.nextInt();
-                    listaAlunos.remover(posicao);
+                    listaAlunos.buscar(posicao);
                     break;
 
                 case 3:
+                    System.out.println("Informe a posicao deseja: ");
+                    posicao = scan.nextInt();
+                    listaAlunos.remover(posicao);
+                    break;
+
+                case 4:
                     listaAlunos.getLista();
+                    break;
+
+                case 5:
+                    System.out.println(listaAlunos.sizeContador());
                     break;
 
                 case 0:
--- src/Vetor.java
+++ src/Vetor.java
@@ -1,38 +1,57 @@
 public class Vetor {
-    Aluno[] aluno = new Aluno[3];
+    Aluno[] aluno = new Aluno[1000];
     private int contador;
 
     public Vetor() {
 
     }
 
-    public void adicionar(Aluno addAluno) {
+    public boolean adicionar(Aluno addAluno) {
         if (contador < aluno.length) {
             aluno[contador] = addAluno;
             contador++;
+            return true;
         }
+        return false;
     }
 
-    public void buscar(int posicao) {
-        if (posicao > 0 && posicao <= aluno.length) {
+    public boolean buscar(int posicao) {
+        if (posicao >= 0 && posicao <= contador) {
             System.out.println(aluno[posicao].getNome());
             System.out.println(aluno[posicao].getNota());
+            return true;
+        } else {
+            System.out.println("O a posição informada esta vazia.");
         }
+        return false;
     }
 
     public void remover(int posicao) {
+        int countTemp;
         if (posicao >= 0 && posicao <= aluno.length) {
             aluno[posicao] = null;
+            
+            for (countTemp = posicao; countTemp < (contador - 1); countTemp++) {
+                aluno[posicao] = aluno[countTemp + 1];
+                if (aluno[posicao] == aluno[posicao + 1]){ 
+                    System.out.println("Igual");
+                }
+            }
+            contador--;
         }
     }
 
     public void getLista() {
-        int contador = 0;
-        for (contador = 0; contador < aluno.length; contador++) {
-            if (aluno[contador] != null) {
-                System.out.println(aluno[contador].getNome());
-                System.out.println(aluno[contador].getNota());
+        int count = 0;
+        for (count = 0; count < contador; count++) {
+            if (aluno[count] != null) {
+                System.out.println(aluno[count].getNome());
+                System.out.println(aluno[count].getNota());
             }   
         }
+    }
+
+    public int sizeContador() {
+        return this.contador;
     }
 }
